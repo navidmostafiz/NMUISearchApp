be97c0fff617510b6e185cd66af517eb8224cbf5
navidmostafiz <navidmostafiz@gmail.com>
2020-04-10 20:57:21 -0400

    Initial Commit

---
+++ NMUISearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NMUISearchApp
+//
+//  Created by Navid on 4/10/20.
+//  Copyright © 2020 UnitedStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
